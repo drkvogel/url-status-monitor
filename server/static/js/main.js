dafbc0f096b7824c61580db5ef7af515ec800b7c
Chris Bird <drkvogel@yahoo.co.uk>
2018-11-24 12:38:36 +0000

    tidy

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -6,14 +6,12 @@
         this.id = id;
         this.name = name;
         this.url = url;
-        this.freq = freq; //?
+        this.freq = freq;
         this.parent = parent;
         this.redOn = false;
         this.flashPeriod = 300;
 
         var self = this;
-
-        // var redLight, grnLight;
 
         this.render = function() {
             var div = document.createElement('div'),
@@ -41,9 +39,7 @@
             this.statusDiv = statusPara;
             statusPara.appendChild(statusText);
             div.appendChild(statusPara);
-            this.div = $(parent).append(div);
-            // this.redLight = object.contentDocument.getElementById('redLight'); // doesn't seem to work at this point
-            // this.grnLight = object.contentDocument.getElementById('grnLight');
+            $(parent).append(div);
         }
 
         function getSVG() {
@@ -95,48 +91,33 @@
         }
     
         function checkURL() {
-            console.log('light id ' + id + ': checkUrl(): ' + url);
             $.ajax({
                 url: url,
                 complete: onComplete,
                 success: onSuccess,
                 error: onError
-                // statusCode: {
-                //     200: function() {
-                //         self.litGrn();
-                //     },
-                //     404: function() {
-                //         self.flashRed();
-                //     }
-                // }
             });
         }
 
         function onSuccess()    { 
-            console.log("light id "+id+" onSuccess");
             self.stopFlash();
             self.litGrn();
         }
+
         function onError()      { 
-            console.log("light id "+id+" onError: url: "+url);
             self.dimGrn();
             self.flashRed();
         }
+
         function onComplete(jqXHR)   { // ajax call is finished, whether successful or not
-            console.log("light id "+id+" onComplete, checked url: " + url+', status: '+jqXHR.status); //  jqXHR: '+JSON.stringify(jqXHR));
-            // $(parent)
-            console.log('statusDiv: ' + JSON.stringify(self.statusDiv));
+            console.log("light id "+id+" onComplete, checked url: "+url+', status: '+jqXHR.status); //  jqXHR: '+JSON.stringify(jqXHR));
             $(self.statusDiv).text('Status: ' + jqXHR.status);
-            
         }
         
-        console.log('StatusPanel(id = ' + id + ', name='+name+', url='+this.url+', freq='+freq+')');
+        console.log('StatusPanel(id = '+id+', name='+name+', url='+this.url+', freq='+freq+')');
         this.render();
-        // console.log('div: ' + JSON.stringify(this.div)); 
-        setTimeout(checkURL, freq * 100);  // just once for testing
-        var interval = freq * 1000;
-        console.log(this.name + ': setInterval: ' + interval);
-        // setInterval(checkURL, interval);
+        // setTimeout(checkURL, freq * 100);  // just once for testing
+        setInterval(checkURL, freq * 1000);
     }
 
     var panels = [];
@@ -159,64 +140,3 @@
 // }());
 
 console.log('main.js ready');
-
-// cruft
-
-    // console.log('$(containerDiv).attr(\'data-id\'): ' + $(containerDiv).attr('data-id'));
-    
-    // var LOCAL = false;
-    // var loc = location.toString().split('://')[1]; // strip off http://, https://
-    // if (loc.substr(0, 9) === 'localhost') { // served locally
-    //     LOCAL = true;
-    // }
-    // console.log('LOCAL: ' + LOCAL);
-
-    // var svg = document.getElementById('svg1');
-    // console.log('got data: ' + data);
-    // console.log('object: ' + i);
-    // var freq = object.freq, url = object.url;
-    // console.log('freq: ' + freq + ', url: '+ url);
-    // $.each(object, function(j, value) {
-        //     console.log(j + '=' + value);
-                // })
-
-    // function onClickCreate() {
-    //     console.log('Create button clicked');
-    //     $('#statusContainer').append(html);
-    // }   
-
-    // function onClickRedButton() {
-    //     console.log('Red button clicked');
-    //     var redLight = svg.contentDocument.getElementById('redLight');
-    //     $(redLight).css('fill', 'white');
-    // }
-    
-    // function onClickGreenButton() {
-    //     console.log('Green button clicked');
-    //     var greenLight = svg.contentDocument.getElementById('grnLight');
-    //     $(greenLight).css('fill', 'white');
-    // }
-
-    // $('#redButton').on('click', onClickRedButton);
-    // $('#greenButton').on('click', onClickGreenButton);
-    // $('#createStatusPanel').on('click', onClickCreate);
-
-    // var html = '<div style="display: inline; float: left" class="statusPanel"><div class="trafficLights" style="text-align: center;">' +
-    //     '<object id="svg1" type="image/svg+xml" data="img/lights.svg">Your browser doesn\'t support objects.</object>' +
-    //     '</div></div>';f
-
-    // StatusPanel.prototype = {
-    //     dimRed: function() {
-    //         $(redLight).css('fill', 'white');
-    //     }
-    // }
-    
-    // function onClickInstantiate() {
-    //     console.log('Instantiate button clicked');
-    //     var panel = new StatusPanel(1, "Test", "http://localhost:8080/status", 10, '#statusContainer');
-    // }
-
-    // $('#instantiateStatusPanel').on('click', onClickInstantiate);
-
-    //$(document).ready(
-        // console.log('Document ready');
